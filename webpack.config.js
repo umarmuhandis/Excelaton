24cff797a8ddd4eb3477fb1a83e01dfdc3a2d419
Umar Engineer <umarmuhandis@gmail.com>
2021-07-07 00:33:22 +0500

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  context: path.resolve(__dirname, "src")
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name.bundle.js]"
+  }
 }
